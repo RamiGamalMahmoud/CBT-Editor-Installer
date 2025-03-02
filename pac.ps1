87b9d0458d1fa75d98c014f6dbf89c83bdca6dff
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-03-02 17:47:39 +0200

    Initial commit

---
+++ pac.ps1
@@ -0,0 +1,10 @@
+dotnet publish -c Release  ..\App\Halaqat\ -r win-x64 -o .\publish
+
+$version = "0.0.1"
+$pack_id = "CBT_Editor"
+$main_exe = "CBT Editor.exe"
+$icon_path = ".\yafea.ico"
+$splash_image = ".\yafea.png"
+$framework = "net8.0-x64-desktop"
+$pack_title = "CBT Editor"
+vpk pack -u $pack_id -v $version -p .\publish -e $main_exe --icon $icon_path --splashImage $splash_image --framework $framework --packTitle $pack_title
