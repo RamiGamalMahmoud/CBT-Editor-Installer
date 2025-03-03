bd7e10679ab0961847f18b588d04b7cf691950bb
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-03-04 00:24:29 +0200

    Create new release 0.0.2

--- pac.ps1
+++ pac.ps1
@@ -1,6 +1,6 @@
-dotnet publish -c Release  ..\App\Halaqat\ -r win-x64 -o .\publish
+dotnet publish -c Release  ..\..\CBT\CBTEditor\CBTEditor\ -r win-x64 -o .\publish
 
-$version = "0.0.1"
+$version = "0.0.2"
 $pack_id = "CBT_Editor"
 $main_exe = "CBT Editor.exe"
 $icon_path = ".\yafea.ico"
