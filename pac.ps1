d558b4b2c589e54c0c4aaab1a85f6872b31f4e37
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-03-04 02:09:37 +0200

    Create new release 0.0.3

--- pac.ps1
+++ pac.ps1
@@ -1,6 +1,6 @@
 dotnet publish -c Release  ..\..\CBT\CBTEditor\CBTEditor\ -r win-x64 -o .\publish
 
-$version = "0.0.2"
+$version = "0.0.3"
 $pack_id = "CBT_Editor"
 $main_exe = "CBT Editor.exe"
 $icon_path = ".\yafea.ico"
