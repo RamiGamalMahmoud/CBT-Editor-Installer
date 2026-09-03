6a3b95352e718ec5c167c41594d269dda01ee54f
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2026-09-04 00:08:50 +0300

    updte

--- pac.ps1
+++ pac.ps1
@@ -1,10 +1,11 @@
-dotnet publish -c Release  ..\CBTEditor\CBTEditor\ -r win-x64 -o .\publish
+dotnet publish -c Release --self-contained true /p:PublishSingleFile=true /p:PublishAot=true  ..\CBTEditor\CBTEditor\ -r win-x64 -o .\publish
+ISCC.exe "CBT Editor Installer Script.iss"
 
-$version = "0.0.20"
-$pack_id = "CBT_Editor"
-$main_exe = "CBT Editor.exe"
-$icon_path = ".\yafea.ico"
-$splash_image = ".\yafea.png"
-$framework = "net8.0-x64-desktop"
-$pack_title = "CBT Editor"
-vpk pack -u $pack_id -v $version -p .\publish -e $main_exe --icon $icon_path --splashImage $splash_image --framework $framework --packTitle $pack_title
+$version = "0.0.21"
+# $pack_id = "CBT_Editor"
+# $main_exe = "CBT Editor.exe"
+# $icon_path = ".\yafea.ico"
+# $splash_image = ".\yafea.png"
+# $framework = "net8.0-x64-desktop"
+# $pack_title = "CBT Editor"
+# vpk pack -u $pack_id -v $version -p .\publish -e $main_exe --icon $icon_path --splashImage $splash_image --framework $framework --packTitle $pack_title
